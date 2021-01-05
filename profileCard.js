e4e68045077de84b95d7ac0cff76cb50b10337fa
SupriyaShekar123 <s.supriya82@gmail.com>
2021-01-05 20:11:36 +0530

    created shawdow dom

--- profileCard.js
+++ profileCard.js
@@ -1,9 +1,18 @@
-//Class with the Constructor
+const tempalate = document.createElement('template');
+tempalate.innerHTML = `<style>
+h3{
+     color:coral;
+}
+</style>`
 
+//Class with the Constructor
 class ProfileCard extends HTMLElement {
     constructor(){
         super();
-        this.innerHTML = `<h3>${this.getAttribute('name')}<h3>`;
+// Creating ShawdowDOM for encapuslization
+        this.attachShadow({mode:'open'})
+        this.shadowRoot.appendChild(tempelate.cloneNode(true))
+        this.innerHTML = `<style>h3{color:coral}</style><h3>${this.getAttribute('name')}<h3>`;
 
     }
 }
